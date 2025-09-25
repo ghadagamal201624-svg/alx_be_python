2a32dc10e7b9c6a8a0925a702ff9845fbce02bb4
Ghada <ghadagamal.201624@gmail.com>
2025-09-25 23:01:59 +0300

    multiplication table gnerator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,16 +4,16 @@ operation = input("choose the operation (+,-,*,/):")
 match operation: 
     case"+":
         result = num1 + num2
-        print("the result is {result}")
+        print(f"the result is {result}")
     case"-":
         result = num1 - num2
-        print("the result is {result}")
+        print(f"the result is {result}")
     case"*":
         result = num1 * num2
-        print("the result is {result}")
+        print(f"the result is {result}")
     case"/":
         if num2 == 0:
             print("Erorr: Division by zero os not allowed!")
         else:
             result = num1 / num2
-            print("the result is {result}")
+            print(f"the result is {result}")

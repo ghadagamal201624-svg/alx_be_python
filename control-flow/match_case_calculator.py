bcc16685bec4c3da0a4315f87c7f9639b4608c80
Ghada <ghadagamal.201624@gmail.com>
2025-09-25 22:34:23 +0300

    calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = input("Enter the first number:")
+num2 = input("enter the second number:")
+operation = input("choose the operation (+,-,*,/):")
+match operation: 
+    case"+":
+        result = num1 + num2
+        print("the result is {result}")
+    case"-":
+        result = num1 - num2
+        print("the result is {result}")
+    case"*":
+        result = num1 * num2
+        print("the result is {result}")
+    case"/":
+        if num2 == 0:
+            print("Erorr: Division by zero os not allowed!")
+        else:
+            result = num1 / num2
+            print("the result is {result}")
---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather = input("what's the weather like to day?(sunny,rainy,cold)")
+if weather == "sunny":
+    print ("wear a t-shirt and sunnglasses")
+elif weather == "rainy":
+    print("dont forget your umbrella and a raincoat")
+elif weather == "cold":
+    print("make sure to wear a warm coat and scarf")
+else:
+    print ("sorry, i dont have any recommendations for this weather")

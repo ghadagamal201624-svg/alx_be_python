398130ca229fa40cc9a1855bc3875bb502012a10
Ghada <ghadagamal.201624@gmail.com>
2025-09-27 20:08:38 +0300

    temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -14,7 +14,7 @@ def main():
     try:
         temprature = float(temp_input)
     except ValueError:
-        print("Invalid temperature. Please enter a numeric value")
+        print("Invalid temperature. Please enter a numeric value.")
         return
     unit = input("Is this temperature in Celsius or Fahrenheit? (C/F):").strip().upper()
     if unit == "C":
